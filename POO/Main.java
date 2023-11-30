cbeae6b56f058b0bb7040f21e6bd7f033ac424b4
Trofu <blincapozos@gmail.com>
2023-11-30 18:22:55 +0100

    1

--- POO/Main.java
+++ POO/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Vehiculo miIbiza = new Vehiculo();
-        miIbiza.marca = "Seat";
-        miIbiza.modelo = "Ibiza";
 
-        System.out.println(miIbiza.marca +" "+miIbiza.modelo);
+        Vehiculo miIbiza = new Vehiculo("Seat","Ibiza","rojo","1235BHC",false,6);
+//        miIbiza.marca = "Seat";
+//        miIbiza.modelo = "Ibiza";
+//
+        System.out.println(miIbiza);
+        Vehiculo miBMW = new Vehiculo("BMW","I3","Negro","5321CBH",true,8);
+        System.out.println(miBMW);
     }
 }
--- POO/Vehiculo.java
+++ POO/Vehiculo.java
@@ -1,13 +1,29 @@
 public class Vehiculo {
 
-    String marca;
-    public String modelo;
-    String color;
-    private int marcha;
-    String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private String matricula;
     private boolean cambioAutomatico;
     private  int numMarchas;
 
 
+
+    public Vehiculo(){
+        marca="";
+        modelo="";
+        color="";
+        matricula="";
+        cambioAutomatico=false;
+        numMarchas=0;
+    }
+    public Vehiculo(String marca,String modelo,String color,String matricula,boolean cambioAuto,int numMarcha){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.color=color;
+        this.matricula=matricula;
+        this.cambioAutomatico=cambioAuto;
+        this.numMarchas=numMarcha;
+    }
 
 }
