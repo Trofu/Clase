7b07f68886d6948016b38d414cfba6d744a5f56f
Trofu <blincapozos@gmail.com>
2023-11-30 17:20:28 +0100

    1

---
+++ POO/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo miIbiza = new Vehiculo();
+        miIbiza.marca = "Seat";
+        miIbiza.modelo = "Ibiza";
+
+        System.out.println(miIbiza.marca +" "+miIbiza.modelo);
+    }
+}
---
+++ POO/Vehiculo.java
@@ -0,0 +1,13 @@
+public class Vehiculo {
+
+    String marca;
+    public String modelo;
+    String color;
+    private int marcha;
+    String matricula;
+    private boolean cambioAutomatico;
+    private  int numMarchas;
+
+
+
+}
