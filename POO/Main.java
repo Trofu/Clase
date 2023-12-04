f3bf95b03093a343e6ed5990b905d1aa308b04f7
Trofu <blincapozos@gmail.com>
2023-12-04 18:40:10 +0100

    1

--- POO/Main.java
+++ POO/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Vehiculo miIbiza = new Vehiculo("Seat","Ibiza","rojo","1235BHC",false);
-        Vehiculo miBMW = new Vehiculo("BMW","I3","Negro","5321CBH",true);
+        Vehiculo miIbiza = new Vehiculo("Seat","Ibiza","rojo","1235BHC",false,3);
+        Vehiculo miBMW = new Vehiculo("BMW","I3","Negro","5321CBH",true,4);
+        Vehiculo miToyota = new Vehiculo("Toyota","Corolla","rojo","9856BXT",false,1);
         System.out.println(miBMW);
+        System.out.println(miToyota.getMarcha());
+        miToyota.subirMarcha();
+        System.out.println(miToyota.getMarcha());
     }
 }
--- POO/Vehiculo.java
+++ POO/Vehiculo.java
@@ -1,5 +1,4 @@
 public class Vehiculo {
-
     private String marca;
     private String modelo;
     private String color;
@@ -7,9 +6,6 @@ public class Vehiculo {
     private String matricula;
     private boolean cambioAutomatico;
     private  int numMarchas;
-
-
-
     public Vehiculo(){
         marca="";
         modelo="";
@@ -18,10 +14,11 @@ public class Vehiculo {
         cambioAutomatico=false;
         numMarchas=0;
     }
-    public Vehiculo(String marca,String modelo,String color,String matricula,boolean cambioAuto){
+    public Vehiculo(String marca,String modelo,String color,String matricula,boolean cambioAuto,int marcha){
         this.marca=marca;
         this.modelo=modelo;
         this.color=color;
+        this.marcha=marcha;
         this.matricula=matricula;
         this.cambioAutomatico=cambioAuto;
         if (cambioAutomatico){
@@ -29,7 +26,47 @@ public class Vehiculo {
         }else{
             this.numMarchas=5;
         }
-
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void settColor(String color){
+        this.color = color;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public String getMatricula(){
+        return matricula;
+    }
+    public void settMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    public int getMarcha(){
+        return  marcha;
+    }
+    public void settMarcha(int marcha){
+        this.marcha = marcha;
+    }
+    public boolean getCambioAutomatico(){
+        return cambioAutomatico;
+    }
+    public void settCambioAutomatico(boolean cambioAutomatico){
+        this.cambioAutomatico = cambioAutomatico;
+    }
+    public int getNumMarcha(){
+        return  numMarchas;
+    }
+    public void settNumMarcha(int numMarcha){
+        this.numMarchas = numMarchas;
     }
 
+    public void subirMarcha(){
+        if (marcha<numMarchas){
+            this.marcha+=1;
+        }
+    }
 }
