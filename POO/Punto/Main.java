fd840ff4305e5c6456d1c2828878289d7cb4e3f9
Trofu <blincapozos@gmail.com>
2023-12-04 20:24:04 +0100

    1

--- POO/Punto/Main.java
+++ POO/Punto/Main.java
@@ -1,4 +1,31 @@
 package Punto;
 
+import java.util.SortedMap;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Punto p2 = new Punto(8);
+        Punto p3 = new Punto(2,8);
+        System.out.println("Punto 1" +p2);
+        System.out.println("Punto 2" +p3);
+        p2.derecha(5);
+        p3.bajar(15);
+        System.out.println("-----------");
+        System.out.println("Punto 1" +p2);
+        System.out.println("Punto 2" +p3);
+        Punto p4 = p3.newPuntoUp(5);
+        Punto p5 = p3.newPuntoDown(48);
+        Punto p6 = p3.newPuntoLeft(36);
+        Punto p7 = p3.newPuntoRight(19);
+        System.out.println("-----------");
+        System.out.println("Punto 3" +p4);
+        System.out.println("Punto 4" +p5);
+        System.out.println("Punto 5" +p6);
+        System.out.println("Punto 6" +p7);
+        System.out.println("Distacia entre Punto 3 y Punto 6 = "+p4.distancia(p7));
+
+
+    }
+
 }
--- POO/Punto/Punto.java
+++ POO/Punto/Punto.java
@@ -2,5 +2,81 @@ package Punto;
 
 public class Punto {
 
+    //Atributos
+    private int x;
+    private int y;
+
+    //Constructores
+    public Punto(){
+        this.x=0;
+        this.y=0;
+    }
+    public Punto(int x){
+        this.x=x;
+        this.y=0;
+    }
+    public Punto(int x, int y){
+        this(x);
+        this.y=y;
+    }
+
+
+    //Getters
+
+    public int getX (){
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    //Metodos
+
+
+    public void subir(int y){
+        this.y+=y;
+    }
+    public void bajar(int y){
+        this.y-=y;
+    }
+    public void derecha(int x){
+        this.x+=x;
+    }
+    public void izquierda(int x){
+        this.x-=x;
+    }
+
+    public Punto newPuntoUp(int distancia){
+        return new Punto(x,y+distancia);
+    }
+    public Punto newPuntoDown(int distancia){
+        return new Punto(x,y-distancia);
+    }
+    public Punto newPuntoLeft(int distancia){
+        return new Punto(x-distancia,y);
+    }
+    public Punto newPuntoRight(int distancia){
+        return new Punto(x+distancia,y);
+    }
+
+    public double distancia(Punto p) {
+//        d=√(x2−x1)2+(y2−y1)2
+        int difx = p.getX() - this.x;
+        int dify = p.getY() - this.y;
+        return Math.sqrt(Math.pow(difx,2)+Math.pow(dify,2));
+    }
+
+
+
+    //toString
+
+
+    @Override
+    public String toString() {
+        return  " (" + x +
+                "," + y +")";
+    }
+
 
 }
