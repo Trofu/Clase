6057689d7c47614ae0f98d656bd5e1ac94accbc0
jespinosac <trofu403@gmail.com>
2024-01-11 17:53:49 +0100

    12

--- Tema 5/Ejercicios OOP/Ejercicio3_Biblioteca/Biblioteca.java
+++ Tema 5/Ejercicios OOP/Ejercicio3_Biblioteca/Biblioteca.java
@@ -9,6 +9,26 @@ public class Biblioteca {
     private String nombre;
     private String localizacion;
 
+    public Biblioteca(String nombre, String localizacion) {
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+        String[][] libro = {
+                {"Don Quijote de la Mancha", "Miguel de Cervantes Saavedra"},
+                {"Cien años de soledad","Gabriel García Márquez"},
+                {"El alquimista", "Paulo Coehlo"},
+                {"20 poemas de amor y una canción desesperada", "Pablo Neruda"},
+                {"La ciudad y los perros", "Mario Vargas Llosa"},
+                {"La casa de los espíritus","Isabel Allende"},
+                {"Desolación", "Gabriela Mistral"},
+                {"Rayuela", "Julio Cortázar"},
+                {"El Aleph", "Jorge Luis Borges"},
+                {"El amor en los tiempos del cólera","Gabriel García Márquez"}
+        };
+        numero_libro_max=libro.length;
+        for (int i = 0; i < libro.length; i++) {
+            libros[i] = new Libro(libro[i][0], libro[i][1], 3);
+        }
+    }
 
     Biblioteca(){
         String[][] libro = {
@@ -63,7 +83,9 @@ public class Biblioteca {
 
     public int recuperarLibro(String titulo){
         for (int i = 0; i < libros.length; i++) {
-            if (parteTitulo(titulo,i)){
+            String tituloLibro = libros[i].getTitulo();
+            tituloLibro= tituloLibro.substring(0,titulo.length());
+            if (tituloLibro.equalsIgnoreCase(titulo)){
                 return i;
             }
         }
@@ -79,51 +101,37 @@ public class Biblioteca {
         }
     }
     public boolean eliminarLibro(String titulo){
-        for (int i = 0; i < numero_libro_max; i++) {
-            if (parteTitulo(titulo,i)){
-                libros[i]=libros[numero_libro_max-1];
-                libros[numero_libro_max-1]=null;
-                numero_libro_max--;
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean prestamo(String titulo){
-        for (int i = 0; i < numero_libro_max; i++) {
-            if (parteTitulo(titulo,i)){
-                if (libros[i].prestamoLibro()){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    public boolean devolucion(String titulo){
-        for (int i = 0; i < numero_libro_max; i++) {
-            if (parteTitulo(titulo,i)){
-                if (libros[i].devolucionLibro()){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    private boolean parteTitulo(String titulo, int i){
-        String tituloLibro = libros[i].getTitulo();
-        tituloLibro= tituloLibro.substring(0,titulo.length());
-        if (tituloLibro.equalsIgnoreCase(titulo)){
+        int pos = recuperarLibro(titulo);
+        if (pos<0){
+            return false;
+        }else if (pos>=0){
+            libros[pos]=libros[numero_libro_max-1];
+            libros[numero_libro_max-1]=null;
+            numero_libro_max--;
             return true;
         }
         return false;
     }
+    public boolean prestamo(String titulo){
+        int pos = recuperarLibro(titulo);
+        if (pos<0){
+            return false;
+        }else {
+            return libros[pos].prestamoLibro();
+        }
+    }
+    public boolean devolucion(String titulo){
+        int pos = recuperarLibro(titulo);
+        if (pos<0){
+            return false;
+        }else {
+            return libros[pos].devolucionLibro();
+        }
+    }
     @Override
     public String toString() {
-        return "Biblioteca{" +
-                "numero_libro_max=" + numero_libro_max +
-                ", libros=" + Arrays.toString(libros) +
-                ", nombre='" + nombre + '\'' +
-                ", localizacion='" + localizacion + '\'' +
-                '}';
+        return "La Biblioteca "+nombre+" de la calle "+localizacion+
+                " tiene "+ numero_libro_max+" de libros.\n"+Arrays.toString(libros)+"\n";
+
     }
 }
--- Tema 5/Ejercicios OOP/Ejercicio3_Biblioteca/BibliotecaTester.java
+++ Tema 5/Ejercicios OOP/Ejercicio3_Biblioteca/BibliotecaTester.java
@@ -5,11 +5,8 @@ public class BibliotecaTester {
     public static void main(String[] args) {
         Biblioteca biblio1 = new Biblioteca();
         Biblioteca biblio2 = new Biblioteca();
-        System.out.println(biblio1+"\n");
-        biblio1.prestamo("20");
-        System.out.println(biblio1);
-        biblio1.devolucion("20");
-        System.out.println(biblio1);
+        biblio1.nuevoLibro()
+
     }
 
 
