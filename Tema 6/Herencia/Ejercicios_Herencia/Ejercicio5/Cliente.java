4ace4e1f24eef7be39ebbeac58a08aecdc004d00
jespinosac <trofu403@gmail.com>
2024-01-17 16:37:42 +0100

    12

---
+++ Tema 6/Herencia/Ejercicios_Herencia/Ejercicio5/Cliente.java
@@ -0,0 +1,7 @@
+package Ejercicios_Herencia.Ejercicio5;
+
+public class Cliente extends Persona{
+    public Cliente(String nombre, String apellidos, String DNI, String direccion) {
+        super(nombre, apellidos, DNI, direccion);
+    }
+}
