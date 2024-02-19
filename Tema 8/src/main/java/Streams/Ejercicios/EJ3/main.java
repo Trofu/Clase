abcdd94b6d3b86dd4ffa176ce9075657a1f75c51
jespinosac <trofu403@gmail.com>
2024-02-19 20:25:20 +0100

    2024-2-19

--- Tema 8/src/main/java/Streams/Ejercicios/EJ3/main.java
+++ Tema 8/src/main/java/Streams/Ejercicios/EJ3/main.java
@@ -10,14 +10,13 @@ public class main {
         String ruta = "Tema 8/src/main/java/Streams/Ejercicios/Documentos/alumnos_notas.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(ruta))){
-            String linea="";
-
+            String linea;
+            double media = 0;
             do {
                 linea = br.readLine();
                 if (linea!=null){
                     String[] partes = linea.split(" ");
                     String nombre = partes[0]+" "+partes[1];
-                    double media = 0;
                     for (int i = 2; i < partes.length; i++) {
                         media += Double.parseDouble(partes[i]);
                     }
---
+++ Tema 8/src/main/java/Streams/Ejercicios/EJ4/main.java
@@ -0,0 +1,61 @@
+package Streams.Ejercicios.EJ4;
+
+import java.io.*;
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        String ruta ="Tema 8/src/main/java/Streams/Ejercicios/Documentos/";
+        String ruta1 = "Tema 8/src/main/java/Streams/Ejercicios/Documentos/usa_nombres.txt";
+        String ruta2 = "Tema 8/src/main/java/Streams/Ejercicios/Documentos/usa_apellidos.txt";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cuantos nombres aleatorios quieres? :");
+        int num = sc.nextInt();
+        System.out.print("Donde quieres guardarlos? :");
+        String fichero = sc.next();
+//        fichero ="usa_personas.txt";
+
+        try (BufferedReader br1 = new BufferedReader(new FileReader(ruta1));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(ruta+fichero));
+             BufferedReader br2 = new BufferedReader(new FileReader(ruta2))){
+
+            List<String> nombre1 = new ArrayList<>();
+            List<String> nombre2 = new ArrayList<>();
+            List<String> nombreCompleto = new ArrayList<>();
+            String nom="",nom2="";
+
+            for (int i = 0; i < num; i++) {
+                nom = br1.readLine();
+                nom2 = br2.readLine();
+                if (nom!=null && nom2!=null){
+                    nombre1.add(nom);
+                    nombre2.add(nom2);
+                }else {
+                    i--;
+                }
+            }
+
+            Collections.shuffle(nombre1);
+            Collections.shuffle(nombre2);
+
+            for (int i = 0; i < num; i++) {
+                nombreCompleto.add(nombre1.get(i)+" "+nombre2.get(i));
+            }
+
+            for (String nombre:nombreCompleto){
+                bw.write(nombre+"\n");
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
+}
